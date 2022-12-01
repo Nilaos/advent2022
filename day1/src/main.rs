d4e5b69224ad8b360b12fa88d698a60206082587
Nicholas Sims <nicholas.d.sims@gmail.com>
2022-12-01 19:00:46 +1100

    day1 complete

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,7 +1,27 @@
 use aoc_driver::*;
 
 fn solution(i: &str) -> String {
-    unimplemented!()
+    let mut v = vec![0];
+    let mut idx = 0;
+
+    for ln in i.split('\n') {
+        if ln.len() <= 1 {
+            v.push(0);
+            idx += 1;
+        } else {
+            v[idx] += ln.parse::<i32>().unwrap();
+        }
+    }
+    v.sort();
+
+    // P1
+    // println!("{}", v.last().unwrap());
+    // v.last().unwrap().to_string()
+
+    // P2
+    let l = v.len();
+    println!("{}", v[l - 3..l].iter().sum::<i32>());
+    v[l - 3..l].iter().sum::<i32>().to_string()
 }
 
 fn main() {
