0d89d85687eb503c958852912dc17ab3b8966e6e
Nicholas Sims <nicholas.d.sims@gmail.com>
2022-12-05 11:16:24 +1100

    much nicer day4 soln

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -12,18 +12,21 @@ fn solution(i: &str) -> String {
     for l in lines {
         let parts = l.split(&[',', '-'][..]).collect::<Vec<&str>>();
 
-        // println!("{:?}", parts);
+        let seta = RangeInclusive::new(
+            parts[0].parse::<i32>().unwrap(),
+            parts[1].parse::<i32>().unwrap(),
+        );
+        let setb = RangeInclusive::new(
+            parts[2].parse::<i32>().unwrap(),
+            parts[3].parse::<i32>().unwrap(),
+        );
 
-        if parts[0].parse::<i32>().unwrap() <= parts[2].parse::<i32>().unwrap()
-            && parts[1].parse::<i32>().unwrap() >= parts[3].parse::<i32>().unwrap()
-        {
+        if seta.contains(setb.start()) && seta.contains(setb.end()) {
             score += 1;
             continue;
         }
 
-        if parts[0].parse::<i32>().unwrap() >= parts[2].parse::<i32>().unwrap()
-            && parts[1].parse::<i32>().unwrap() <= parts[3].parse::<i32>().unwrap()
-        {
+        if setb.contains(seta.start()) && setb.contains(seta.start()) {
             score += 1;
             continue;
         }
@@ -46,33 +49,58 @@ fn solution2(i: &str) -> String {
 
         // println!("{:?}", parts);
 
-        if parts[0].parse::<i32>().unwrap() <= parts[2].parse::<i32>().unwrap()
-            && parts[1].parse::<i32>().unwrap() >= parts[3].parse::<i32>().unwrap()
-        {
+        let seta = RangeInclusive::new(
+            parts[0].parse::<i32>().unwrap(),
+            parts[1].parse::<i32>().unwrap(),
+        );
+        let setb = RangeInclusive::new(
+            parts[2].parse::<i32>().unwrap(),
+            parts[3].parse::<i32>().unwrap(),
+        );
+
+        // if seta.any(|v| setb.contains(&v)) {
+        //     score += 1;
+        //     continue;
+        // }
+
+        if seta.contains(setb.start()) || seta.contains(setb.end()) {
             score += 1;
             continue;
         }
 
-        if parts[0].parse::<i32>().unwrap() >= parts[2].parse::<i32>().unwrap()
-            && parts[1].parse::<i32>().unwrap() <= parts[3].parse::<i32>().unwrap()
-        {
+        if setb.contains(seta.start()) || setb.contains(seta.start()) {
             score += 1;
             continue;
         }
+        // panic!();
 
-        if parts[0].parse::<i32>().unwrap() >= parts[2].parse::<i32>().unwrap()
-            && parts[0].parse::<i32>().unwrap() <= parts[3].parse::<i32>().unwrap()
-        {
-            score += 1;
-            continue;
-        }
+        // if parts[0].parse::<i32>().unwrap() <= parts[2].parse::<i32>().unwrap()
+        //     && parts[1].parse::<i32>().unwrap() >= parts[3].parse::<i32>().unwrap()
+        // {
+        //     score += 1;
+        //     continue;
+        // }
 
-        if parts[1].parse::<i32>().unwrap() >= parts[2].parse::<i32>().unwrap()
-            && parts[1].parse::<i32>().unwrap() <= parts[3].parse::<i32>().unwrap()
-        {
-            score += 1;
-            continue;
-        }
+        // if parts[0].parse::<i32>().unwrap() >= parts[2].parse::<i32>().unwrap()
+        //     && parts[1].parse::<i32>().unwrap() <= parts[3].parse::<i32>().unwrap()
+        // {
+        //     score += 1;
+        //     continue;
+        // }
+
+        // if parts[0].parse::<i32>().unwrap() >= parts[2].parse::<i32>().unwrap()
+        //     && parts[0].parse::<i32>().unwrap() <= parts[3].parse::<i32>().unwrap()
+        // {
+        //     score += 1;
+        //     continue;
+        // }
+
+        // if parts[1].parse::<i32>().unwrap() >= parts[2].parse::<i32>().unwrap()
+        //     && parts[1].parse::<i32>().unwrap() <= parts[3].parse::<i32>().unwrap()
+        // {
+        //     score += 1;
+        //     continue;
+        // }
     }
 
     println!("{score}");
